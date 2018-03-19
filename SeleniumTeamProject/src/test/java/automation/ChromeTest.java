367bad2ed6a716d0bcea61ff08cf098be849293a
Aspiricity <michael@aspiricity.com>
2018-03-19 13:53:45 -0600

    Added Logging and Reporting to Base Level Project

--- SeleniumTeamProject/src/test/java/automation/ChromeTest.java
+++ SeleniumTeamProject/src/test/java/automation/ChromeTest.java
@@ -5,17 +5,23 @@ import org.testng.annotations.BeforeTest;
 
 import java.util.concurrent.TimeUnit;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
 import org.testng.annotations.AfterTest;
 
 public class ChromeTest {
 
+	private static final Logger logger = LogManager.getLogger(ChromeTest.class);
 	private WebDriver driver;
 	
 	@Test
 	public void loadSiteTest() {
 		driver.get("https://damp-garden-55551.herokuapp.com/");
+		Reporter.log("Site loaded successfully.");
+		logger.info("Website loaded.");
 	}
 	
 	@BeforeTest
